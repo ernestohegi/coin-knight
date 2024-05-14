e00352092fd14bacc394fcdaf0fc9d9c4efeecea
Ernesto Hegi <ernesto.hegi@gmail.com>
2024-05-14 14:34:33 +0100

    tidy up

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,10 +2,8 @@ extends Node
 
 var score = 0
 
-@onready var score_text = %Score
 @onready var ui = %UI
 
 func add_point():
 	score += 1
-	score_text.text = str(score) + " coins"
 	ui.update_score(score)
